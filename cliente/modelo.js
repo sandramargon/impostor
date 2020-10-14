98a356d01da65e9c692fcfa97f304b07a8838295
SANDRA MARTÍNEZ GONZÁLEZ <Sandra.Martinez20@alu.uclm.es>
2020-10-15 00:04:34 +0200

    arreglos tras la sesion miercoles

--- cliente/modelo.js
+++ cliente/modelo.js
@@ -1,8 +1,8 @@
 function Juego(){
 	this.partidas={};
-	this.crearPartida=function(num,owner){ 
+	this.crearPartida=function(num,owner){
 		let codigo=this.obtenerCodigo();
-		if(!this.partidas[codigo]){
+		if (!this.partidas[codigo]){
 			this.partidas[codigo]=new Partida(num,owner.nick);
 			owner.partida=this.partidas[codigo];
 		}
@@ -13,6 +13,7 @@ function Juego(){
 			this.partidas[codigo].agregarUsuario(nick);
 		}
 	}
+	
 	this.obtenerCodigo=function(){
 		let cadena="ABCDEFGHIJKLMNOPQRSTUVXYZ";
 		let letras=cadena.split('');
@@ -25,16 +26,14 @@ function Juego(){
 	}
 }
 
-
-function Partida(num, owner){
+function Partida(num,owner){
 	this.maximo=num;
 	this.nickOwner=owner;
 	this.fase=new Inicial();
-	this.usuarios={} //versión array asociativo
+	this.usuarios={};
 	this.agregarUsuario=function(nick){
-		this.fase.agregarUsuario(nick,this);
+		this.fase.agregarUsuario(nick,this)
 	}
-
 	this.puedeAgregarUsuario=function(nick){
 		let nuevo=nick;
 		let contador=1;
@@ -43,17 +42,16 @@ function Partida(num, owner){
 			contador=contador+1;
 		}
 		this.usuarios[nuevo]=new Usuario(nuevo);
-		this.comprobarMinimo();
+		//this.comprobarMinimo();
 	}
 	this.comprobarMinimo=function(){
-		return Object.keys(this.usuarios).length>=4;
+		return Object.keys(this.usuarios).length>=4
 	}
 	this.comprobarMaximo=function(){
-		return Object.keys(this.usuarios).length<=this.maximo;
+		return Object.keys(this.usuarios).length<this.maximo
 	}
-			
 	this.iniciarPartida=function(){
-			this.fase.iniciarPartida(this);
+		this.fase.iniciarPartida(this);
 	}
 	this.abandonarPartida=function(nick){
 		this.fase.abandonarPartida(nick,this);
@@ -61,24 +59,23 @@ function Partida(num, owner){
 	this.eliminarUsuario=function(nick){
 		delete this.usuarios[nick];
 	}
-
 	this.agregarUsuario(owner);
 }
-
 
 function Inicial(){
 	this.nombre="inicial";
 	this.agregarUsuario=function(nick,partida){
 		partida.puedeAgregarUsuario(nick);
+		if (partida.comprobarMinimo()){
+			partida.fase=new Completado();
+		}		
 	}
 	this.iniciarPartida=function(partida){
 		console.log("Faltan jugadores");
 	}
 	this.abandonarPartida=function(nick,partida){
 		partida.eliminarUsuario(nick);
-		if(partida.comprobarMinimo()){
-			partida.fase=new Completado();
-		}
+		//comprobar si no quedan usr
 	}
 }
 
@@ -88,13 +85,16 @@ function Completado(){
 		partida.fase=new Jugando();
 	}
 	this.agregarUsuario=function(nick,partida){
-		if(partida.comprobarMaximo()){
+		if (partida.comprobarMaximo()){
 			partida.puedeAgregarUsuario(nick);
+		}
+		else{
+			console.log("Lo siento, numero máximo")
 		}
 	}
 	this.abandonarPartida=function(nick,partida){
 		partida.eliminarUsuario(nick);
-		if(!partida.comprobarMinimo()){
+		if (!partida.comprobarMinimo()){
 			partida.fase=new Inicial();
 		}
 	}
@@ -106,21 +106,19 @@ function Jugando(){
 		console.log("La partida ya ha comenzado");
 	}
 	this.iniciarPartida=function(partida){
-
 	}
 	this.abandonarPartida=function(nick,partida){
-		//comprobar si termina la partida
 		partida.eliminarUsuario(nick);
+		//comprobar si termina la partida
 	}
 }
 
 function Final(){
-	this.nombre="final";
+	this.final="final";
 	this.agregarUsuario=function(nick,partida){
 		console.log("La partida ha terminado");
 	}
 	this.iniciarPartida=function(partida){
-		
 	}
 	this.abandonarPartida=function(nick,partida){
 		//esto es absurdo
@@ -138,10 +136,10 @@ function Usuario(nick,juego){
 		this.partida.iniciarPartida();
 	}
 	this.abandonarPartida=function(){
-		this.partida.
+		this.partida.abandonarPartida(this.nick);
 	}
 }
 
 function randomInt(low, high) {
 	return Math.floor(Math.random() * (high - low) + low);
-} 
+}
--- cliente/spec/modeloSpec.js
+++ cliente/spec/modeloSpec.js
@@ -3,24 +3,61 @@ describe("El juego del impostor", function() {
   var usr;
 
   beforeEach(function() {
-  	juego= new Juego();
+  	juego=new Juego();
   	usr=new Usuario("Pepe",juego);
   });
 
-  it("inicialmente... ", function() {
-	expect(Object.keys(juego.partidas).length).toEqual(0);
-	expect(usr.nick).toEqual("Pepe");
-	expect(usr.juego).not.toBe(undefined);
-	});
+  it("comprobar valores iniciales del juego", function() {
+  	expect(Object.keys(juego.partidas).length).toEqual(0);
+  	expect(usr.nick).toEqual("Pepe");
+  	expect(usr.juego).not.toBe(undefined);
+  });
 
-  it("el usr Pepe crear un partida de 4 jugadores", function(){
-  	var codigo=usr.crearPartida(4);
-  	expect(codigo).not.toBe(undefined);
-  	expect(juego.partidas[codigo].nickOwner).toEqual(usr.nick);
-  	expect(juego.partidas[codigo].maximo).toEqual(4);
-  	expect(juego.partidas[codigo].fase.nombre).toEqual("inicial");
-	});
-  it("varios usuarios se unen a la partida",function())
+  describe("el usr Pepe crea una partida de 4 jugadores",function(){
+	var codigo;
+	beforeEach(function() {
+	  	codigo=usr.crearPartida(4);
+	  });
 
+	it("se comprueba la partida",function(){ 	
+	  	expect(codigo).not.toBe(undefined);
+	  	expect(juego.partidas[codigo].nickOwner).toEqual(usr.nick);
+	  	expect(juego.partidas[codigo].maximo).toEqual(4);
+	  	expect(juego.partidas[codigo].fase.nombre).toEqual("inicial");
+	 	var num=Object.keys(juego.partidas[codigo].usuarios).length;
+	  	expect(num).toEqual(1);
+	  });
 
+	it("varios usuarios se unen a la partida",function(){
+		juego.unirAPartida(codigo,"ana");
+	  	var num=Object.keys(juego.partidas[codigo].usuarios).length;
+	  	expect(num).toEqual(2);
+		expect(juego.partidas[codigo].fase.nombre).toEqual("inicial");
+		juego.unirAPartida(codigo,"isa");
+	  	var num=Object.keys(juego.partidas[codigo].usuarios).length;
+	  	expect(num).toEqual(3);
+		expect(juego.partidas[codigo].fase.nombre).toEqual("inicial");	  	
+		juego.unirAPartida(codigo,"tomas");
+	  	var num=Object.keys(juego.partidas[codigo].usuarios).length;
+	  	expect(num).toEqual(4);
+		expect(juego.partidas[codigo].fase.nombre).toEqual("completado");
+	  });
+
+	it("Pepe inicia la partida",function(){
+		juego.unirAPartida(codigo,"ana");
+	  	var num=Object.keys(juego.partidas[codigo].usuarios).length;
+	  	expect(num).toEqual(2);
+		expect(juego.partidas[codigo].fase.nombre).toEqual("inicial");
+		juego.unirAPartida(codigo,"isa");
+	  	var num=Object.keys(juego.partidas[codigo].usuarios).length;
+	  	expect(num).toEqual(3);
+		expect(juego.partidas[codigo].fase.nombre).toEqual("inicial");	  	
+		juego.unirAPartida(codigo,"tomas");
+	  	var num=Object.keys(juego.partidas[codigo].usuarios).length;
+	  	expect(num).toEqual(4);
+		expect(juego.partidas[codigo].fase.nombre).toEqual("completado");		
+		usr.iniciarPartida();
+		expect(juego.partidas[codigo].fase.nombre).toEqual("jugando");
+	})
+   });
 })
