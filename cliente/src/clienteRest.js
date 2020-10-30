354450550d1a001d79461a86048a6646c2598118
SANDRA MARTÍNEZ GONZÁLEZ <Sandra.Martinez20@alu.uclm.es>
2020-10-30 21:47:51 +0100

    función callback en crearPartida

--- cliente/src/clienteRest.js
+++ cliente/src/clienteRest.js
@@ -1,17 +1,50 @@
 function clienteRest(){
-	this.crearPartida=function(num,owner){
+
+	this.crearPartida=function(num,owner,callback){
 		$.getJSON("/crearPartida/"+num+"/"+owner,function(data){    
     		console.log(data);
+    		callback(data);
 		});
+	}
 
 	this.unirAPartida=function(nick,codigo){
 		$.getJSON("/unirAPartida/"+nick+"/"+codigo,function(data){    
     		console.log(data);
 		});
+	}
 
 	this.listaPartidas=function(){
 		$.getJSON("/listaPartidas",function(lista){    
     		console.log(lista);
 		});
+	}
 
-}}}}
+	this.iniciarPartida=function(nick,codigo){
+		$.getJSON("/iniciarPartida/"+nick+"/"+codigo,function(data){    
+    		console.log(data);
+		});
+	}
+}
+
+function pruebas(){
+	var codigo=undefined;
+	rest.crearPartida(3,"pepe",function(data){
+		codigo=data.codigo;		
+	});
+	rest.crearPartida(4,"pepe",function(data){
+		codigo=data.codigo;
+		rest.unirAPartida("juan",codigo);
+		rest.unirAPartida("juani",codigo);
+		rest.unirAPartida("juana",codigo);
+		rest.unirAPartida("juanma",codigo);
+	});
+	rest.crearPartida(5,"pepe",function(data){
+		codigo=data.codigo;
+		rest.unirAPartida("juan",codigo);
+		rest.unirAPartida("juani",codigo);
+		rest.unirAPartida("juana",codigo);
+		rest.unirAPartida("juanma",codigo);
+	});
+	
+//agregar otras partidas de 6, 7… hasta 10 jugadores
+}
